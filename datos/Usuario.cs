ab82971a1bd461f286b2362956c789c9e7d4068f
isaac vidal gutierrez <isaac_v@outlook.es>
2021-11-10 03:35:16 -0300

    Agregar archivos de proyecto.

---
+++ datos/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinformEvaluacion3.modelo;
+
+namespace WinformEvaluacion3.datos
+{
+    public class Usuario
+    {
+        List<Usuariomodel> lista = new List<Usuariomodel>();
+
+        public void Guardar(Usuariomodel modelo)
+        {
+
+            lista.Add(modelo);
+        }
+            public List<Usuariomodel> Consultar() {
+
+            return lista;
+        
+        }
+
+    }
+}
